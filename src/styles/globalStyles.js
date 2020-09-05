fe0f45231576fc61084708792e678cbce5255c1d
ferueda <fe.rueda@outlook.com>
2020-09-05 12:45:02 -0400

    Add self hosted fonts
    Added self hosted Bitter and Montserrat fonts.
    Removed  link tag font import from index.html
    Added font-face import to globalStyles.js

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,6 +1,46 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Montserrat';
+    font-weight: 400;
+    font-style: normal;
+    src: url("/fonts/Montserrat-Regular.woff") format("woff"),
+      url("/fonts/Montserrat-Regular.woff2") format("woff2");
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-weight: 500;
+    font-style: normal;
+    src: url("/fonts/Montserrat-Medium.woff") format("woff"),
+      url("/fonts/Montserrat-Medium.woff2") format("woff2");
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-weight: 600;
+    font-style: normal;
+    src: url("/fonts/Montserrat-SemiBold.woff") format("woff"),
+      url("/fonts/Montserrat-SemiBold.woff2") format("woff2");
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-weight: 700;
+    font-style: normal;
+    src: url("/fonts/Montserrat-Bold.woff") format("woff"),
+      url("/fonts/Montserrat-Bold.woff2") format("woff2");
+  }
+
+  @font-face {
+    font-family: 'Bitter';
+    font-weight: 400;
+    font-style: normal;
+    src: url("/fonts/Bitter-Regular.woff") format("woff"),
+      url("/fonts/Bitter-Regular.woff2") format("woff2");
+  }
+
   :root {
     --titles-font: 'Bitter', sans-serif;
     --main-font: 'Montserrat', sans-serif;
