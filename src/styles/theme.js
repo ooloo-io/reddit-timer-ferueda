3fe37439ea24737d2eede5bce3f8b419a9b1e7aa
ferueda <fe.rueda@outlook.com>
2020-09-05 12:47:08 -0400

    Create theme.js
    File that contains the styled-components theme

---
+++ src/styles/theme.js
@@ -0,0 +1,23 @@
+const theme = {
+  color: {
+    text: 'var(--gray-base)',
+    headline: 'var(--main-black)',
+    link: 'var(--brownish-grey)',
+  },
+  font: {
+    family: {
+      default: 'var(--default-font)',
+      headline: 'var(--headline -font)',
+    },
+    size: {
+      default: '16px',
+      headline: '38px',
+      sectionHeader: '24px',
+    },
+    lineHeight: {
+      default: 1.69,
+    },
+  },
+};
+
+export default theme;
