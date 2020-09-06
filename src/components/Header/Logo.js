23cbd1caeb044f1b179fa20d57bc2f60b91a9256
ferueda <fe.rueda@outlook.com>
2020-09-05 21:14:11 -0400

    Update Header Logo.js
    Updated the styled Wrapper name

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -3,16 +3,16 @@ import styled from 'styled-components';
 
 import { ReactComponent as ReactLogo } from './logo.svg';
 
-const StyledDiv = styled.div`
+const Wrapper = styled.div`
   width: 150px;
   height: 36px;
   object-fit: contain;
 `;
 
 const Logo = () => (
-  <StyledDiv>
+  <Wrapper role="img" aria-label="reddit timer logo">
     <ReactLogo />
-  </StyledDiv>
+  </Wrapper>
 );
 
 export default Logo;
