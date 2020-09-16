7be0ed10af037635aadd1be74100fd98af2b11a2
ferueda <fe.rueda@outlook.com>
2020-09-16 15:46:26 -0300

    Update Cypress search query tests

--- cypress/integration/6-SubredditForm.spec.js
+++ cypress/integration/6-SubredditForm.spec.js
@@ -2,29 +2,22 @@ import describeOnBranches from '../utils/describeOnBranches';
 
 describeOnBranches('subreddit-form')('Subreddit Form', () => {
   it('Updates URL with input value on submit', () => {
-    cy.visit('/search/javascript');
+    cy.visit('/search?subreddit=javascript');
 
-    cy.get('input')
-      .clear()
-      .type('reactjs');
+    cy.get('input').clear().type('reactjs');
 
-    cy.contains('button', 'SEARCH', { matchCase: false })
-      .click();
+    cy.contains('button', 'SEARCH', { matchCase: false }).click();
 
-    cy.url().should('equal', `${Cypress.config().baseUrl}/search/reactjs`);
+    cy.url().should('equal', `${Cypress.config().baseUrl}/search?subreddit=reactjs`);
   });
 
   it('Input value updates when header link is clicked', () => {
-    cy.visit('/search/reactjs');
+    cy.visit('/search?subreddit=reactjs');
 
-    cy.get('input')
-      .should('have.value', 'reactjs');
+    cy.get('input').should('have.value', 'reactjs');
 
-    cy.get('header')
-      .contains('Search')
-      .click();
+    cy.get('header').contains('Search').click();
 
-    cy.get('input')
-      .should('have.value', 'javascript');
+    cy.get('input').should('have.value', 'javascript');
   });
 });
