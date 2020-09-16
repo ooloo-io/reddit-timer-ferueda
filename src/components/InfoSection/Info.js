633104dd68a12ed8fc4db439f94c89ab976a92b8
ferueda <fe.rueda@outlook.com>
2020-09-15 22:18:50 -0300

    Updated InfoSection.js and create Info.js
    Extracted into its own component each article from the InfoSection component.

---
+++ src/components/InfoSection/Info.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.article`
+  margin: 105px auto 0 auto;
+  width: 738px;
+  min-height: 81px;
+`;
+
+const Heading = styled.h2`
+  text-align: left;
+`;
+
+const Content = styled.div`
+  margin-top: 13px;
+`;
+
+const Info = ({ heading, children }) => {
+  return (
+    <Wrapper>
+      <Heading>{heading}</Heading>
+      <Content>{children}</Content>
+    </Wrapper>
+  );
+};
+
+Info.propTypes = {
+  children: PropTypes.string.isRequired,
+  heading: PropTypes.string.isRequired,
+};
+
+export default Info;
--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -4,43 +4,15 @@ import styled from 'styled-components';
 
 import * as ROUTES from '../../constants/routes';
 
-const Wrapper = styled.article`
+import Info from './Info';
+
+const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 100%;
-  margin: 0 auto;
+  margin: 28px auto 0 auto;
   padding: 0 20px;
-`;
-
-const Row = styled.div`
-  margin: 0 auto;
-  width: 738px;
-  min-height: 81px;
-`;
-
-const FirstRow = styled(Row)`
-  margin-top: 133px;
-`;
-
-const SecondRow = styled(Row)`
-  margin-top: 105px;
-`;
-
-const SectionHeader = styled.h2`
-  text-align: left;
-`;
-
-const List = styled.ul`
-  margin-top: 13px;
-`;
-
-const ListItem = styled.li`
-  line-height: ${({ theme }) => theme.font.lineHeight.default};
-`;
-
-const Text = styled.p`
-  margin-top: 13px;
 `;
 
 const scrollToSection = (el) => el.scrollIntoView({ behavior: 'smooth', block: 'center' });
@@ -63,32 +35,26 @@ const InfoSection = () => {
 
   return (
     <Wrapper>
-      <FirstRow>
-        <SectionHeader ref={firstRowRef}>How it works</SectionHeader>
-        <List>
-          <ListItem>• We find the 500 top posts from the past year for a subreddit.</ListItem>
-          <ListItem>
-            • The data is visualized in a heatmap grouped by weekday and hour of the day.
-          </ListItem>
-          <ListItem>• See immediately when to submit your reddit post.</ListItem>
-        </List>
-      </FirstRow>
+      <Info heading="How it works">
+        • We find the 500 top posts from the past year for a subreddit.
+        <br />
+        • The data is visualized in a heatmap grouped by weekday and hour of the day.
+        <br />
+        • See immediately when to submit your reddit post.
+      </Info>
 
-      <SecondRow>
-        <SectionHeader ref={secondRowRef}>About</SectionHeader>
-        <Text>
-          This app was created during a course on&nbsp;
-          <a href={ROUTES.OOLOO} target="_blank" rel="noopener noreferrer">
-            ooloo.io
-          </a>
-          &nbsp;with the goal to implement a pixel-perfect real-world application with professional
-          workflows and tools like Kanban, ClickUp, Zeplin, GitHub, pull requests and code
-          reviews.&nbsp;
-          <a href={`${ROUTES.OOLOO}/employers`} target="_blank" rel="noopener noreferrer">
-            Click here for more information.
-          </a>
-        </Text>
-      </SecondRow>
+      <Info heading="About">
+        This app was created during a course on&nbsp;
+        <a href={ROUTES.OOLOO} target="_blank" rel="noopener noreferrer">
+          ooloo.io
+        </a>
+        &nbsp;with the goal to implement a pixel-perfect real-world application with professional
+        workflows and tools like Kanban, ClickUp, Zeplin, GitHub, pull requests and code
+        reviews.&nbsp;
+        <a href={`${ROUTES.OOLOO}/employers`} target="_blank" rel="noopener noreferrer">
+          Click here for more information.
+        </a>
+      </Info>
     </Wrapper>
   );
 };
