6886ae4f11960e56ad381ae54be0e7cd8737039c
ferueda <fe.rueda@outlook.com>
2020-09-18 12:02:02 -0300

    Update Search.test.js
    Removed unnecesary setup code.

--- src/__tests__/Search.test.js
+++ src/__tests__/Search.test.js
@@ -2,30 +2,19 @@ import React from 'react';
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import fetchMock from 'jest-fetch-mock';
-import { MemoryRouter, Route } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 import mockResponse from '../__mocks__/subreddit-javascript-response.json';
 
 fetchMock.enableMocks();
 
 const setup = (initialPath = '/search?subreddit=javascript') => {
-  let history;
-
   const app = (
     <MemoryRouter initialEntries={[initialPath]}>
       <App />
-      <Route
-        path="*"
-        render={(props) => {
-          history = props.history;
-          return null;
-        }}
-      />
     </MemoryRouter>
   );
   render(app);
-
-  return { history };
 };
 
 describe('Search Page', () => {
