b8c1f483a2572bc79ecafd98182ce4fbc28f4c47
ferueda <fe.rueda@outlook.com>
2020-09-20 15:11:56 -0300

    Create Table.js
    Created the component that renders the table part of the Heatmap

---
+++ src/components/Heatmap/Table.js
@@ -0,0 +1,72 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { useTable } from 'react-table';
+
+const StyledTable = styled.table`
+  grid-area: Table;
+`;
+
+const TdButton = styled.button`
+  width: 40px;
+  height: 40px;
+  background-color: ${({ col, theme }) => theme.heatmap.colors[col >= 10 ? 10 : col]};
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 0.64;
+  letter-spacing: normal;
+  text-align: center;
+  vertical-align: middle;
+  color: #ffffff;
+  border: none;
+  margin: 0;
+  padding: 0;
+
+  &:hover,
+  &:focus {
+    outline: 1px solid #1e2537;
+    outline-offset: -1px;
+  }
+`;
+
+const Table = ({ columns, data }) => {
+  // eslint-disable-next-line object-curly-newline
+  const { getTableProps, getTableBodyProps, rows, prepareRow } = useTable({
+    columns,
+    data,
+  });
+
+  return (
+    <StyledTable {...getTableProps()}>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                const { key, role } = cell.getCellProps();
+
+                return (
+                  <td key={key} role={role}>
+                    <TdButton col={cell.value} type="button">
+                      {cell.render('Cell')}
+                    </TdButton>
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </StyledTable>
+  );
+};
+
+Table.propTypes = {
+  columns: PropTypes.arrayOf(PropTypes.object).isRequired,
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default Table;
