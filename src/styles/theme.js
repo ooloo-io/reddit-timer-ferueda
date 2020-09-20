62e6407cbc8d8f455ef1072f88afa908cbc77c65
ferueda <fe.rueda@outlook.com>
2020-09-20 15:09:28 -0300

    Update theme.js
    Added heatmap colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -40,6 +40,21 @@ const theme = {
     fontWeight: 500,
     height: '36px',
   },
+  heatmap: {
+    colors: {
+      0: '#e0e592',
+      1: '#aed396',
+      2: '#a9d194',
+      3: '#a0ce93',
+      4: '#99cd94',
+      5: '#8cc894',
+      6: '#5eb391',
+      7: '#5db492',
+      8: '#5cb391',
+      9: '#5aad8c',
+      10: '#3984a3',
+    },
+  },
 };
 
 export default theme;
