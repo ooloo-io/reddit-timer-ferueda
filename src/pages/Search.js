0359a9e176a667e3f5532fa10927949cdf073257
ferueda <fe.rueda@outlook.com>
2020-09-20 15:14:02 -0300

    Update Search.js
    Added the Heatmap component to the Search page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 import SubredditForm from '../components/SubredditForm/SubredditForm';
+import Heatmap from '../components/Heatmap/Heatmap';
 import Loading from '../components/Loading/Loading';
 import ErrorMessage from '../components/shared/ErrorMessage';
 
@@ -9,13 +10,15 @@ import useSubredditPosts from '../hooks/useSubredditPosts';
 
 const Search = () => {
   const location = useLocation();
-  const { isLoading, error } = useSubredditPosts(location.search.replace(/\?subreddit=/i, ''));
+  const { posts, isLoading, error } = useSubredditPosts(
+    location.search.replace(/\?subreddit=/i, ''),
+  );
 
   return (
     <>
       <SubredditForm />
 
-      {isLoading === true ? <Loading /> : null}
+      {isLoading === true ? <Loading /> : <Heatmap posts={posts} />}
 
       {error !== null ? <ErrorMessage /> : null}
     </>
