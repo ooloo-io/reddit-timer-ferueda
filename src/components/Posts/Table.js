7768e0baf187c47a211a9e4b2d7672318fe8aa87
ferueda <fe.rueda@outlook.com>
2020-09-29 15:53:15 -0300

    Create Posts' Table.js

---
+++ src/components/Posts/Table.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useTable } from 'react-table';
+
+const Table = () => {
+  return (
+    <table>
+      <thead></thead>
+    </table>
+  );
+};
+
+export default Table;
